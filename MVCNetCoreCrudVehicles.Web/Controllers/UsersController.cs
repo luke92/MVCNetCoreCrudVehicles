1947a5cf2e471feffaeb86388f10d4945ee5e311
Lucas Vargas <lucasjv92@gmail.com>
2020-07-29 10:03:56 -0300

    Add files via upload

---
+++ MVCNetCoreCrudVehicles.Web/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCNetCoreCrudVehicles.Business.Modules.Owner;
+
+namespace MVCNetCoreCrudVehicles.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly IOwnerModule _ownerModule;
+
+        public UsersController(IOwnerModule ownerModule)
+        {
+            _ownerModule = ownerModule;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Get()
+        {
+            var owners = await _ownerModule.GetOwners();
+
+            return Ok(owners);
+        }
+    }
+}
